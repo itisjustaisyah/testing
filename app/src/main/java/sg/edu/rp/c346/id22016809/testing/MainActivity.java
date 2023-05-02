9f90766b641f7e4d0392cf300e4cd37eb1a32125
NUUR AISYAH BINTE FAROUK <22016809@myrp.edu.sg>
2023-05-02 09:58:29 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22016809/testing/MainActivity.java
@@ -0,0 +1,13 @@
+package sg.edu.rp.c346.id22016809.testing;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
